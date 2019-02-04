606b86468e8041938ea06a88cff5a932000c504b
TofflerDev <baraban.ms@gmail.com>
2019-02-04 13:56:35 +0300

    iplemented core logic

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -2,6 +2,13 @@ class MainPageController < ApplicationController
     def start
     end
     def search
-        @query = params[:q]
+        client = Octokit::Client.new(:login => Rails.application.credentials.github[:login], :password => Rails.application.credentials.github[:password])
+        begin
+            @repo = Octokit::Repository.from_url(params[:q])
+        rescue
+            flash[:alert] = "Can't reach repo"
+        else
+            @contributors = client.contribs(@repo).first(3)
+        end    
     end
 end
